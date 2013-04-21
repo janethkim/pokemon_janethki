5887b35fd21806546120fcfbc822e6558f117c18
Janet Kim <janethki@usc.edu>
2013-04-20 19:12:47 -0700

    The class for the background class that will slide along. Can now slide. issue #6. close #2

---
+++ background.cpp
@@ -0,0 +1,22 @@
+#include "background.h"
+#include <cstdlib>
+
+Background::Background(QPixmap* pixMap_, double x_, double y_)
+  : Thing(pixMap_, x_, y_)
+{
+  vx = 0.5;
+  vy = 0;
+}
+
+void Background::move(int xmax, int ymax)
+{
+  x -= vx;
+  if ( abs(x) >= xmax )
+    x = xmax;
+  y -= vy;
+  if ( abs(y) > ymax )
+    y = ymax;
+    
+  setPos(x, y);
+  
+}
---
+++ background.h
@@ -0,0 +1,14 @@
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+#include "thing.h"
+
+class Background : public Thing {
+public:
+  Background(QPixmap *pixMap_, double x_, double y_);
+  ~Background() {}
+  void move(int xmax, int ymax);
+
+};
+
+#endif
