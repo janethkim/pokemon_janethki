354c0f2af922dbebb772325a18cd952569149679
Janet Kim <janethki@usc.edu>
2013-04-20 19:14:43 -0700

    Implemented functions to handle the timer and have the background slide.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,13 +4,40 @@
 MainWindow::MainWindow()
 {
   window = new QWidget;
+  window->setFixedSize( WINDOW_MAX_X, WINDOW_MAX_Y );
   scene = new QGraphicsScene;
-  view = new QGraphicsView(scene);
-  timer = new QTimer;
+  view = new QGraphicsView(scene, window);
+  view->setFixedSize( WINDOW_MAX_X, WINDOW_MAX_Y );
+  timer = new QTimer(this);
+  bgPic = new QPixmap("Images/game-background.jpg");
+  bg_1 = new Background(bgPic, 0, 0);
+  bg_2 = new Background(bgPic, WINDOW_MAX_X, 0);
+  scene->addItem( bg_1 );
+  scene->addItem( bg_2 );
+  
+  view->setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+  view->setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+  
+  timer->setInterval(5);
+  connect(timer, SIGNAL(timeout()), this, SLOT(handleTimer())); 
+//  window->addWidget( view );
 }
 
 MainWindow::~MainWindow()
 {
+  timer->stop();
   delete window;
   delete timer;
+}
+
+void MainWindow::show()
+{
+  timer->start();
+  window->show();
+}
+
+void MainWindow::handleTimer()
+{
+  bg_1->move(WINDOW_MAX_X, WINDOW_MAX_Y); 
+  bg_2->move(WINDOW_MAX_X, WINDOW_MAX_Y);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,21 +3,29 @@
 
 #include <QMainWindow>
 #include <QTimer>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include "background.h"
 
-#define WINDOW_MAX_X 250
-#define WINDOW_MAX_Y 250
+#define WINDOW_MAX_X 800
+#define WINDOW_MAX_Y 500
 
 class MainWindow : public QWidget {
   Q_OBJECT
 public:
-  MainWindow();
+  explicit MainWindow();
   ~MainWindow();
+  void show();
   
 private:
   QWidget *window;
   QTimer *timer;
-  QGraphicScene *scene;
+  QGraphicsScene *scene;
   QGraphicsView *view;
+  Background *bg_1;
+  Background *bg_2;
+  QPixmap* bgPic;
   
 public slots:
   void handleTimer();
