666b789ee6dd450a24dea504cba348c9f9916000
Janet Kim <janethki@usc.edu>
2013-04-25 03:14:58 -0700

    Made the background the gameplay background. The start button leads to the actual game, although I will have to prompt the user for a name. The quit button will quit the application. issue #16

--- startscreen.cpp
+++ startscreen.cpp
@@ -1,15 +1,47 @@
 #include "startscreen.h"
+#include <QPainter>
 
-StartScreen::StartScreen(QWidget *parent, MainWindow *m) : QWidget(parent), main(m)
+StartScreen::StartScreen(QWidget *parent, QPixmap *bg_, MainWindow *m)
+  : QGraphicsView(parent), main(m), bg(bg_)
 {
-  start = new QPushButton("START");
+//  QPalette p = this->palette();
+//  p.setBrush(QPalette::Base, *bg);
+//  this->setPalette(p);
+//  parent->setStyleSheet("background-image: url(./Images/game-background.jpg)");
+//  setPaletteBackgroundPixmap(*bg);
+  scene = new QGraphicsScene;
+  setScene(scene);
+  background = new QGraphicsPixmapItem( *bg );
+  scene->addItem(background);
+//  QPixmap* newGame = new QPixmap("./Images/start_scale.png");
+  start = new QPushButton( "START");
+  
+//  start = new Start( newGame, 200, 150, this );
+//  quit = new Quit( newGame, 300, 150, this );
+//  
+//  scene->addItem(start);
+//  scene->addItem(quit);
+ 
+ 
+
+  
+  buttons = new QWidget(this);
   start->setFixedSize(70, 30);
   quit = new QPushButton("QUIT");
+
   quit->setFixedSize(70,30);
+
   layout = new QHBoxLayout;
   layout->addWidget(start);
   layout->addWidget(quit);
-  setLayout(layout);
+ 
+  buttons->setLayout(layout);
+  buttons->setGeometry(QRect(250,150, 300, 200));
+  grid = new QGraphicsRectItem(0, 0, 800, 500);
+  scene->setSceneRect(grid->rect());
+  
+  setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+  setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
   
   connect(start, SIGNAL(clicked()), this, SLOT(handleStart()));
 }
@@ -23,3 +55,15 @@ QPushButton* StartScreen::getQuit()
 {
   return quit;
 }
+
+//void StartScreen::handleQuit()
+//{
+//  a->quit();
+//}
+
+//void StartScreen::paintEvent(QPaintEvent *paint)
+//{
+//  QPainter paint(this);
+//  paint.drawPixmap(0,0,*bg);
+//  QWidget::paintEvent(*paint);
+//}
--- startscreen.h
+++ startscreen.h
@@ -5,22 +5,48 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 #include "mainwindow.h"
+#include <QPalette>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+//#include "startbutton.h"
+//#include "start.h"
+//#include "quit.h"
+
+//class StartButton;
+//class Start;
+//class Quit;
 
 class MainWindow;
 
-class StartScreen : public QWidget
+class StartScreen : public QGraphicsView
 {
   Q_OBJECT
 public:
-  StartScreen(QWidget *parent, MainWindow *m);
+  StartScreen(QWidget *parent, QPixmap *bg_, MainWindow *m);
   QPushButton* getQuit();
+//  void handleStart();
+//  void handleQuit();
+//  void paintEvent(QPaintEvent *);
   
 private:
   QPushButton *start, *quit;
+//  Start *start;
+//  Quit *quit;
   MainWindow *main;
+  QPixmap* newGame;
+  QGraphicsScene* scene;
+  QPixmap *bg;
+  QGraphicsPixmapItem* background;
+  QGraphicsRectItem* grid;
   QHBoxLayout *layout;
+  QPaintEvent *p;
+  QWidget *buttons;
+//  QApplication *a;
+  
+  
 public slots:
   void handleStart();
+//  void handleQuit();
  
   
 };
