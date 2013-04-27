709b6ff5157c68b854d60b48fb7bf3bbe15355c9
Janet Kim <janethki@usc.edu>
2013-04-26 17:54:28 -0700

    Doxygen documentation for username.h. issue #20

--- username.h
+++ username.h
@@ -11,24 +11,48 @@
 
 class MainWindow;
 
+/** The Username class is the screen where the user will enter their name.
+ *  @author Janet Kim */
 class Username : public QGraphicsView
 {
   Q_OBJECT
 public:
+  /** The constructor for Username. Calls the QGraphicsView constructor.
+   *  @param parent The parent QWidget
+   *  @param bg_ The picture of the background
+   *  @param m A pointer to the MainWindow so it can call its functions*/
   Username(QWidget *parent, QPixmap *bg_, MainWindow *m);
 private:
+  /** The button to press after entering your username */
   QPushButton* enter;
+  
+  /** The text box where the player can enter his/her name */
   QLineEdit* name;
+  
+  /** The pointer to the MainWindow being displayed */
   MainWindow *main;
+  
+  /** The scene which this view will hold */
   QGraphicsScene* scene;
+  
+  /** A pointer to the pixMap of the background */
   QPixmap *bg;
+  
+  /** The item that holds the picture of the background */
   QGraphicsPixmapItem* background;
+  
+  /** A grid to regulate the size of the scene */
   QGraphicsRectItem* grid;
+  
+  /** The layout within the view that places the button and 
+   *  text box in a vertical order */
   QVBoxLayout *layout;
 //  QPaintEvent *p;
   QWidget *overall;
   
 public slots:
+  /** The handleEnter() slot that will proceed to the main gameplay
+   *  if the input was valid. */
   void handleEnter();
 
 };
